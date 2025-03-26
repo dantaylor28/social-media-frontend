f428b7cf5b1c0f0b0df439934c326108e39e5654
Dan <dan.taylor1493@gmail.com>
2025-03-26 07:43:01 +0100

    Update baseurl test

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://social-media-api-25-e478cbb9dc3f.herokuapp.com/";
+// axios.defaults.baseURL = "https://social-media-api-25-e478cbb9dc3f.herokuapp.com/";
+axios.defaults.baseURL = "drf-api-2024-5-1a986cf1c53f.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
